bc63b4ad2ac3112a8f0b9dfaa8e33d707edf3f7a
Soares Walisson <68417371+Costabe@users.noreply.github.com>
2022-04-18 12:19:22 +0200

    Update Padroes_Comportamentais.py

--- Padroes_Comportamentais.py
+++ Padroes_Comportamentais.py
@@ -2,40 +2,20 @@ from __future__ import annotations
 from abc import ABC, abstractmethod
 from typing import List
 
+# O Visitor é um padrão de projeto comportamental que permite que você separe algoritmos dos objetos nos quais eles operam.
 
 class Component(ABC):
-    """
-    The Component interface declares an `accept` method that should take the
-    base visitor interface as an argument.
-    """
-
-    @abstractmethod
+    
     def accept(self, visitor: Visitor) -> None:
         pass
 
-
-class ConcreteComponentA(Component):
-    """
-    Each Concrete Component must implement the `accept` method in such a way
-    that it calls the visitor's method corresponding to the component's class.
-    """
-
+class ConcreteComponentA(Component)
     def accept(self, visitor: Visitor) -> None:
-        """
-        Note that we're calling `visitConcreteComponentA`, which matches the
-        current class name. This way we let the visitor know the class of the
-        component it works with.
-        """
-
+       
         visitor.visit_concrete_component_a(self)
 
     def exclusive_method_of_concrete_component_a(self) -> str:
-        """
-        Concrete Components may have special methods that don't exist in their
-        base class or interface. The Visitor is still able to use these methods
-        since it's aware of the component's concrete class.
-        """
-
+        
         return "A"
 
 
@@ -52,31 +32,13 @@ class ConcreteComponentB(Component):
 
 
 class Visitor(ABC):
-    """
-    The Visitor Interface declares a set of visiting methods that correspond to
-    component classes. The signature of a visiting method allows the visitor to
-    identify the exact class of the component that it's dealing with.
-    """
 
-    @abstractmethod
+    
     def visit_concrete_component_a(self, element: ConcreteComponentA) -> None:
         pass
 
-    @abstractmethod
     def visit_concrete_component_b(self, element: ConcreteComponentB) -> None:
         pass
-
-
-"""
-Concrete Visitors implement several versions of the same algorithm, which can
-work with all concrete component classes.
-
-You can experience the biggest benefit of the Visitor pattern when using it with
-a complex object structure, such as a Composite tree. In this case, it might be
-helpful to store some intermediate state of the algorithm while executing
-visitor's methods over various objects of the structure.
-"""
-
 
 class ConcreteVisitor1(Visitor):
     def visit_concrete_component_a(self, element) -> None:
@@ -95,17 +57,11 @@ class ConcreteVisitor2(Visitor):
 
 
 def client_code(components: List[Component], visitor: Visitor) -> None:
-    """
-    The client code can run visitor operations over any set of elements without
-    figuring out their concrete classes. The accept operation directs a call to
-    the appropriate operation in the visitor object.
-    """
-
-    # ...
+    
     for component in components:
         component.accept(visitor)
     # ...
-
+#Solucao do Codigo..
 
 if __name__ == "__main__":
     components = [ConcreteComponentA(), ConcreteComponentB()]
